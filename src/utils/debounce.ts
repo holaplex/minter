5c58c93c180f084a522f9773de86be2a87ecf29c
Austin Milt <austin@holaplex.com>
2022-07-13 10:19:52 -0700

    remove unnecessary arg check in Debounce

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,13 +1,10 @@
 export class Debounce {
   typingTimeout: null | ReturnType<typeof setTimeout> = null;
 
-  debounceFcn = (callback: ()=>void, timeoutDuration = 900) => {
-    if (!callback) {
-      throw new Error('missing argument callback');
-    }
+  debounceFcn = (callback: () => void, timeoutDuration = 900) => {
     if (this.typingTimeout) {
       clearTimeout(this.typingTimeout);
     }
-    this.typingTimeout = setTimeout(callback, timeoutDuration)
-  }
+    this.typingTimeout = setTimeout(callback, timeoutDuration);
+  };
 }
